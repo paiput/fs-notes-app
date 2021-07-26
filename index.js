8bdf61ad12447b868f895148a13ced3f97ac9f57
Lucas Piputto <lucaspiputto@gmail.com>
2021-07-26 16:05:38 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+];
+
+// const requestLogger = (req, res, next) => {
+//   console.log('Method:', req.method);
+//   console.log('Path:', req.path);
+//   console.log('Body:', req.body);
+//   console.log('---');
+//   next();
+// };
+
+// app.use(requestLogger);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World again</h1>');
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(note => note.id))
+    : 0;
+  return maxId + 1;
+};
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    });
+  };
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  };
+
+  notes.concat(note);
+
+  res.json(note);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  res.status(202).end();
+});
+
+// const unknownEndpoint = (req, res) => {
+//   res.status(404).send({ error: 'unknown endpoint' });
+// };
+
+// app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

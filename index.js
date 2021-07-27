f051cc9d6ebf9d680d55b20383809495bda34cab
Lucas Piputto <lucaspiputto@gmail.com>
2021-07-27 12:09:52 -0300

    Add unknown endpoint

--- index.js
+++ index.js
@@ -89,11 +89,11 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(202).end();
 });
 
-// const unknownEndpoint = (req, res) => {
-//   res.status(404).send({ error: 'unknown endpoint' });
-// };
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
 
-// app.use(unknownEndpoint);
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {

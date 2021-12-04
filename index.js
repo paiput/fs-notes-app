ad2844a7d0048525dcb55508e64c60484688c7e8
Lucas Piputto <lucaspiputto@gmail.com>
2021-12-04 17:51:05 -0300

    Delete unnecesary code

--- index.js
+++ index.js
@@ -8,8 +8,3 @@ const server = http.createServer(app);
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});

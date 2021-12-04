8f997f41cce340f2dc97fe8f62de3768daebeb8f
Lucas Piputto <lucaspiputto@gmail.com>
2021-12-04 14:24:39 -0300

    Adapt index.js to the new project structure

--- index.js
+++ index.js
@@ -1,115 +1,13 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const Note = require('./models/note');
+const app = require('./app');
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.use(express.static('build'));
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-const url = process.env.MONGODB_URI;
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error);
-  });
-
-const requestuestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:', request.path);
-  console.log('Body:', request.body);
-  console.log('---');
-  next();
-};
-
-app.use(requestuestLogger);
-
-app.get('/api/notes', (request, response, next) => {
-  Note.find({})
-    .then(notes => {
-      response.json(notes);
-    })
-    .catch(error => next(error));
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch(error => next(error));
-});
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({ error: 'content missing' });
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date()
-  });
-
-  note.save()
-    .then(savedNote => savedNote.toJSON())
-    .then(savedAndFormattedNote => {
-      response.json(savedAndFormattedNote);
-    }) 
-    .catch(error => next(error));
-});
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
-
-  const note = {
-    content: body.content,
-    important: body.important
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote);
-    })
-    .catch(error => next(error));
-});
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch(error => next(error));
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'Casterror') {
-    return response.status(400).send({ error: 'malformatted id' });
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {

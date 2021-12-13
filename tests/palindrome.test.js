6890f07b28282393532fb354737691299c87df24
Lucas Piputto <lucaspiputto@gmail.com>
2021-12-13 09:05:28 -0300

    Introduction to testing

---
+++ tests/palindrome.test.js
@@ -0,0 +1,19 @@
+const { palindrome } = require('../utils/for_testing');
+
+test('palindrome of a', () => {
+  const result = palindrome('a');
+
+  expect(result).toBe('a');
+});
+
+test('palindrome of react', () => {
+  const result = palindrome('react');
+
+  expect(result).toBe('tkaer');
+});
+
+test('palindrome of releveler', () => {
+  const result = palindrome('releveler');
+
+  expect(result).toBe('releveler');
+});
